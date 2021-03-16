6cddaeace1fbe5bd0eb6e6f9a7b1fecadf553ef2
freesgen <jesusant.guerrero@gmail.com>
2021-03-16 01:25:35 -0400

    Exploring components

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [mode, setMode] = useState('zen');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.header}> 
+        <Text style={styles.header}>Zen.</Text>
+      </View>
+      <View style={{ flex: 5 }}>
+        <Text> { mode } </Text>
+      </View>
+      <View style={styles.footer}>
+        <TouchableOpacity  onPress={() => setMode('zen')} style={styles.button}>
+          <Text> Zen </Text>
+        </TouchableOpacity>
+        <TouchableOpacity  onPress={() => setMode('lineup')} style={styles.button}>
+          <Text> Lineup </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -12,8 +27,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column",
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  footer: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    height: 20,
+    width: "100%"
+  },
+  button: {
+    width: 100,
+    minWidth: "48%",
+    color: "white",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 50,
+    fontSize: 36
+  },
+  header: {
+    backgroundColor: "red",
+    borderBottomColor: "#333",
+    borderBottomWidth: 2,
+    height: 40,
+    fontSize: 24,
+    width: "100%"
+  }
 });
