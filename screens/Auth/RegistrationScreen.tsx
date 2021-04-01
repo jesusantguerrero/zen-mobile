9c3060dd3ba3d1b225538ccdef64a461bbd09d48
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-04-01 03:12:33 -0400

    Add login prototype and firebase

---
+++ screens/Auth/RegistrationScreen.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import styles from '../RegistrationScreen/styles';
+
+export default function RegistrationScreen({navigation}) {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+
+    const onFooterLinkPress = () => {
+        navigation.navigate('Login')
+    }
+
+    const onSignupPress = () => {
+        if (password != confirmPassword) {
+            alert("Passwords dont match");
+            return;
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <KeyboardAwareScrollView
+                style={{ flex: 1, width: '100%' }}
+                keyboardShouldPersistTaps="always">
+                <Image
+                    style={styles.logo}
+                    source={require('../../assets/logo.png')}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='E-mail'
+                    placeholderTextColor="#aaaaaa"
+                    onChangeText={(text) => setEmail(text)}
+                    value={email}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholderTextColor="#aaaaaa"
+                    secureTextEntry
+                    placeholder='Password'
+                    onChangeText={(text) => setPassword(text)}
+                    value={password}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholderTextColor="#aaaaaa"
+                    secureTextEntry
+                    placeholder='Password'
+                    onChangeText={(text) => setConfirmPassword(text)}
+                    value={confirmPassword}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => onSignupPress()}>
+                    <Text style={styles.buttonTitle}>Register</Text>
+                </TouchableOpacity>
+                <View style={styles.footerView}>
+                    <Text style={styles.footerText}>Alredy have an account? <Text onPress={onFooterLinkPress} style={styles.footerLink}>Login</Text></Text>
+                </View>
+            </KeyboardAwareScrollView>
+        </View>
+    )
+}
