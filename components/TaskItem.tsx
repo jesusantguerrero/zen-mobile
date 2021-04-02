abcbeca460969957e550002238f08468930a3a2b
Jesus Guerrero <jesusant.guerrero@gmail.com>
2021-04-02 02:24:06 -0400

    Add bare tracker component

--- components/TaskItem.tsx
+++ components/TaskItem.tsx
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: "row",
         justifyContent: "space-between",
-        borderBottomColor: '#dedede',
+        borderBottomColor: '#cccccc',
         borderBottomWidth: 1,
         paddingBottom: 16,
         width: "100%"
